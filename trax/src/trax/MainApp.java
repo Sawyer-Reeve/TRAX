886f32f0f4841a668544c57794c3910ea44a45a8
Daytrippr <60251063+Daytrippr@users.noreply.github.com>
2024-04-06 16:53:21 -0600

    Update MainApp.java

--- trax/src/trax/MainApp.java
+++ trax/src/trax/MainApp.java
@@ -1,14 +1,71 @@
 package trax;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		//initializing
-		RailLine BlueLine = new RailLine("Blue Line",);
+	/*	RailLine BlueLine = new RailLine("Blue Line",);
 		RailLine RedLine = new RailLine("Red Line",);
 		RailLine GreenLine = new RailLine("Green Line",);
-		RailLine FrontRunner = new RailLine("FrontRunner",);
-
+		RailLine FrontRunner = new RailLine("FrontRunner",);*/
+		
+		//Blue Line Stations
+		ArrayList<String> BlueStations = new ArrayList<String>(Arrays.asList("Salt Lake Central Station",
+				"Old Greektown Station",
+				"Planetarium Station",
+				"Arena Station",
+				"Temple Square Station",
+				"City Center Station",
+				"Gallivan Plaza Station",
+				"Courthouse Station",
+				"600 South Station",
+				"900 South Station",
+				"Ballpark Station",
+				"Central Pointe Station",
+				"Millcreek Station",
+				"Meadowbrook Station",
+				"Murray North Station",
+				"Murray Central Station",
+				"Fashion Place West Station",
+				"Midvale Fort Union Station",
+				"Midvale Center Station",
+				"Historic Sandy Station",
+				"Sandy Expo Station",
+				"Sandy Civic Center Station",
+				"Crescent View Station",
+				"Kimballs Lane Station",
+				"Draper Town"));
+		ArrayList<String> RedStations = new ArrayList<String>(Arrays.asList("U. Of U. Medical Center Station",
+				"Fort Douglas Station University South Campus Station",
+				"Stadium Station",
+				"900 East Station",
+				"Trolley Station",
+				"Library Station",
+				"Courthouse Station",
+				"600 South Station",
+				"900 South Station",
+				"Ballpark Station",
+				"Central Pointe Station",
+				"Millcreek Station",
+				"Meadowbrook Station",
+				"Murray North Station",
+				"Murray Central Station",
+				"Fashion Place West Station",
+				"Bingham Junction Station",
+				"Historic Gardner Station",
+				"West Jordan City Center Station",
+				"2700 W Sugar Factory Rd Station",
+				"Jordan Valley Station",
+				"4800 W Old Bingham Hwy Station",
+				"5600 W Old Bingham Hwy Station",
+				"South Jordan Parkway Station",
+				"Daybreak Parkway Station"));
+			
 	}
+	
+	
 
 }
