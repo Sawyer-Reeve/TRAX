92aebd5c9f5b1d0c4dc636a6c1248cf640bab156
darnsandnoble <67134498+darnsandnoble@users.noreply.github.com>
2024-04-19 00:41:42 -0600

    Resizing pins brah

--- trax/src/trax/ImagePanel.java
+++ trax/src/trax/ImagePanel.java
@@ -1,6 +1,8 @@
 package trax;
 
 import java.awt.Graphics;
+import java.awt.Image;
+
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
@@ -9,7 +11,7 @@ public class ImagePanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	private String backgroundImg = "/Resources/TRAX MAP.png";
-	private String pinImg = "/Resources/PinImage.png";
+	private String pinImg = "/Resources/PinDrop.png";
 	
 	private int x = 0;
 	private int y = 0;
@@ -28,12 +30,15 @@ public class ImagePanel extends JPanel {
 		background.paintIcon(this,  g, 0, 0);
 		
 		ImageIcon pin = new ImageIcon(ImagePanel.class.getResource(pinImg));
-		pin.paintIcon(this, g,  x, y);
+		Image image = pin.getImage();		
+		Image temp = image.getScaledInstance(40, -1, Image.SCALE_SMOOTH);
+		ImageIcon scaledPin = new ImageIcon(temp);
+		scaledPin.paintIcon(this, g, x, y);
 	}
 	
 	public void setPinXY(int x, int y) {
-		this.x=x;
-		this.y=y;
+		this.x = x;
+		this.y = y;
 	}
 	
 }
